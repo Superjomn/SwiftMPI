370883b2d1a28c32a149e06297fe48fedb348d67
Superjom <superjom@gmail.com>
2015-07-06 21:53:08 +0800

    change doc2vec to sent2vec

--- src/apps/word2vec/run.sh
+++ src/apps/word2vec/run.sh
@@ -1,3 +1,4 @@
 #../../../third/local/bin/mpirun -np 5 
-../../../third/local/bin/mpirun -np 1  -cpu-set 0-15 ./bin/word2vec -config demo.conf -data ~/data.sample.txt -niters 10 -output 1.param 
+#../../../third/local/bin/mpirun -np 1  -cpu-set 0-15 ./bin/word2vec -config demo.conf -data ~/data.sample.txt -niters 10 -output 1.param 
+./bin/word2vec -config demo.conf -data ./data.sample.txt -niters 10 -output 1.param 
 #./bin/word2vec -config demo.conf -data ~/data.sample.txt -niters 1 -output 1.param
